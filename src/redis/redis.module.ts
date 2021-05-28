b57d16bd51c95455e37ccb70516041bf7d1cb440
исраел едет <jtad009@gmail.com>
2021-05-28 20:30:17 +0100

    removes redis port

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -10,7 +10,7 @@ import * as redisStore from 'cache-manager-redis-store';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         store: redisStore,
-        host: configService.get('REDIS_URL'),
+        host: configService.get('REDIS_TLS_URL'),
         // port: configService.get('REDIS_PORT'),
         ttl: configService.get('CACHE_TTL'),
         max: configService.get('MAX_ITEM_IN_CACHE'),
