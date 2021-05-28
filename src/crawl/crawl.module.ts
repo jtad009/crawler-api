588d5bd7807f54861d6a367518e3f2c7c53e69c2
исраел едет <jtad009@gmail.com>
2021-05-28 11:54:30 +0100

    safely adds the redis without errors

--- src/crawl/crawl.module.ts
+++ src/crawl/crawl.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CrawlService } from './crawl.service';
 import { CrawlResolver } from './crawl.resolver';
+import { RedisModule } from 'src/redis/redis.module';
+import { RedisService } from 'src/redis/redis.service';
 
 @Module({
+  imports: [RedisModule],
   providers: [CrawlService, CrawlResolver],
 })
 export class CrawlModule {}
--- src/crawl/crawl.resolver.ts
+++ src/crawl/crawl.resolver.ts
@@ -1,11 +1,15 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
+import { RedisService } from 'src/redis/redis.service';
 import { CrawlService } from './crawl.service';
 import { GetUrlArgs } from './dto/args/get-url-args';
 import { Meta } from './meta.entity';
 
 @Resolver((of) => Meta)
 export class CrawlResolver {
-  constructor(private readonly metaService: CrawlService) {}
+  constructor(
+    private readonly metaService: CrawlService,
+    private cacheManager: RedisService,
+  ) {}
 
   @Query(() => Meta)
   async getMetas(@Args() urlArgs: GetUrlArgs): Promise<Meta> {
